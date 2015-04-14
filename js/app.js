8511b2d1319be6253e84f3c157c6398fde0ebe3c
bensky <ben@krokosky.com>
2015-04-14 16:03:33 -0400

    Made the bugs enter random rows with random speeds

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 // Enemies our player must avoid
-var Enemy = function(home_row) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    //Start the bug all the way to the left
     this.x = -101;
+    //Pick a random row
+    home_row = Math.floor((Math.random() * 3) + 1);
     this.y = (home_row * 83) - 20;
+    //Pick a random speed
+    this.speed = Math.floor((Math.random() * 5) + 1);
 }
 
 // Update the enemy's position, required method for game
@@ -17,9 +22,13 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     numCols = 5;
-    this.x++;
-    if(this.x >= (numCols+1)*101)
+    this.x = this.x + this.speed;
+    //If the bug has reached the edge, then wrap it and pick a new row
+    if(this.x >= (numCols+1)*101) {
         this.x = -101;
+        home_row = Math.floor((Math.random() * 3) + 1);
+        this.y = (home_row * 83) - 20;      
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -35,7 +44,7 @@ var Player = function(start_col,start_row){
     this.col = start_col;
     this.row = start_row;
     this.x = this.col * 101;
-    this.y = this.row * 83;
+    this.y = (this.row * 83) - 20;
 }
 
 Player.prototype.update = function(dt){
@@ -48,22 +57,22 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(input) {
     switch(input) {
         case 'left':
-            if(this.col>0) {
+            if(this.col > 0) {
                 this.col--;
             }
             break;
         case 'right':
-            if(this.col<4) {
+            if(this.col < 4) {
                 this.col++;
             }
             break; 
         case 'up':
-            if(this.row>0) {
+            if(this.row > 0) {
                 this.row--;
             }
             break;    
         case 'down':
-            if(this.row<5) {
+            if(this.row < 5) {
                 this.row++;
             }
             break;    
@@ -71,16 +80,16 @@ Player.prototype.handleInput = function(input) {
             break;
     }
     this.x = this.col * 101;
-    this.y = this.row * 83;   
+    this.y = (this.row * 83) - 20;   
 }
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var bug1 = new Enemy("1");
-var bug2 = new Enemy("2");
-var bug3 = new Enemy("3");
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
 
 var allEnemies = [bug1,bug2,bug3];
 
